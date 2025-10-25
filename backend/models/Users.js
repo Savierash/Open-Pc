72f0c5e0e1bc1281d9af7c8457b8123ea0f598fe
Paul Justine <114402930+Jecchisauce@users.noreply.github.com>
2025-10-25 08:47:32 +0800

    new

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -5,8 +5,10 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
+
+  // added for OTP functionality
+  otp: { type: String, default: null },
+  otpExpires: { type: Date, default: null },
 }, { timestamps: true });
 
-// Export model from this file. Consumers should `require('../models/users')`.
 module.exports = mongoose.model('User', UserSchema);
-  
